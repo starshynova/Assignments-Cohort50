3bbe330ca4cbe9899cc9e4b19789743a0a152676
starshynova <l.starshynova@gmail.com>
2024-11-08 14:39:53 +0100

    ex3-ready

--- 3-UsingAPIs/Week2/assignment/ex3-rollAnAce.js
+++ 3-UsingAPIs/Week2/assignment/ex3-rollAnAce.js
@@ -1,14 +1,3 @@
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-UsingAPIs/Week2/README.md#exercise-3-roll-an-ace
-
-1. Run the unmodified exercise and observe that it works as advertised. Observe 
-   that the die must be thrown an indeterminate number of times until we get an 
-   ACE or until it rolls off the table.
-2. Now, rewrite the body of the `rollDieUntil()` function using async/await and 
-   without using recursion. Hint: a `while` loop may come handy.
-3. Refactor the function `main()` to use async/await and try/catch.
-------------------------------------------------------------------------------*/
-// ! Do not change or remove the next two lines
 import { rollDie } from '../../helpers/pokerDiceRoller.js';
 /** @import {DieFace} from "../../helpers/pokerDiceRoller.js" */
 
@@ -17,24 +6,24 @@ import { rollDie } from '../../helpers/pokerDiceRoller.js';
  * @param {DieFace} desiredValue
  * @returns {Promise<DieFace>}
  */
-export function rollDieUntil(desiredValue) {
-  // TODO rewrite this function using async/await
-  return rollDie().then((value) => {
-    if (value !== desiredValue) {
-      return rollDieUntil(desiredValue);
+export async function rollDieUntil(desiredValue) {
+  let value;
+  do {
+    value = await rollDie();
+  } while (value !== desiredValue);
+  return value;
+}
+
+async function main() {
+  try {
+    if (await rollDieUntil('ACE')) {
+      (results) => console.log('Resolved!', results);
     }
-    return value;
-  });
+  } catch (error) {
+    console.log('Rejected!', error.message);
+  }
 }
 
-// TODO refactor this function to use try/catch
-function main() {
-  rollDieUntil('ACE')
-    .then((results) => console.log('Resolved!', results))
-    .catch((error) => console.log('Rejected!', error.message));
-}
-
-// ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
   main();
 }
